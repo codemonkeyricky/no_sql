6dbbd63e8f0e8770751ad1c57f0360ab7aea4dfd
Richard <code.monkey.ricky@gmail.com>
2024-11-17 17:20:21 -0800

    wip

---
+++ server.cc
@@ -0,0 +1,53 @@
+#include "node.hh"
+#include <iomanip>
+#include <iostream>
+
+#include <boost/asio/co_spawn.hpp>
+#include <boost/asio/detached.hpp>
+#include <boost/asio/io_context.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/signal_set.hpp>
+#include <boost/asio/write.hpp>
+
+using boost::asio::awaitable;
+using boost::asio::co_spawn;
+using boost::asio::detached;
+using boost::asio::use_awaitable;
+using boost::asio::ip::tcp;
+namespace this_coro = boost::asio::this_coro;
+
+using namespace std;
+
+awaitable<void> rx_process(tcp::socket socket) {
+
+    char payload[1024];
+    for (;;) {
+        std::size_t n = co_await socket.async_read_some(
+            boost::asio::buffer(payload), boost::asio::use_awaitable);
+
+        co_await async_write(socket, boost::asio::buffer(payload, n),
+                             boost::asio::use_awaitable);
+    }
+}
+
+awaitable<void> listener() {
+    auto executor = co_await this_coro::executor;
+
+    tcp::acceptor acceptor(executor, {tcp::v4(), 55555});
+    for (;;) {
+        auto socket =
+            co_await acceptor.async_accept(boost::asio::use_awaitable);
+        co_spawn(executor, rx_process(std::move(socket)), detached);
+    }
+}
+
+int main() {
+    boost::asio::io_context io_context(1);
+    boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
+
+    signals.async_wait([&](auto, auto) { io_context.stop(); });
+
+    co_spawn(io_context, listener(), detached);
+
+    io_context.run();
+}
