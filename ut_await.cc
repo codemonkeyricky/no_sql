d5e2768389abcb416984ff8c3c75df22927fe35f
Richard <code.monkey.ricky@gmail.com>
2024-11-25 09:15:15 -0800

    wip

--- ut_await.cc
+++ ut_await.cc
@@ -19,9 +19,12 @@
 
 using namespace std;
 
-map<string, shared_ptr<Node>> nodes;
+using Cluster = map<string, shared_ptr<Node>>;
 
-boost::cobalt::task<void> rx_process(Node& node,
+/* shared_ptr to cluster */
+static shared_ptr<Cluster> cluster;
+
+boost::cobalt::task<void> rx_process(shared_ptr<Node> node,
                                      boost::asio::ip::tcp::socket socket) {
 
     for (;;) {
@@ -36,7 +39,7 @@ boost::cobalt::task<void> rx_process(Node& node,
         if (cmd == "r") {
             /* read */
             auto key = string(data + 2, n - 2);
-            auto value = co_await node.read(key);
+            auto value = co_await node->read(key);
             auto resp = "ra:" + value;
             co_await boost::asio::async_write(
                 socket, boost::asio::buffer(resp.c_str(), resp.size()),
@@ -51,7 +54,7 @@ boost::cobalt::task<void> rx_process(Node& node,
             auto k = kv.substr(0, p);
             auto v = kv.substr(p + 1);
 
-            co_await node.write(k, v, cmd == "w");
+            co_await node->write(k, v, cmd == "w");
 
             string resp;
             if (cmd == "wf")
@@ -65,7 +68,7 @@ boost::cobalt::task<void> rx_process(Node& node,
         } else if (cmd == "g") {
             /* gossip */
             auto gossip = payload.substr(p + 1);
-            node.gossip_rx(gossip);
+            node->gossip_rx(gossip);
             auto resp = "ga:" + gossip;
             co_await boost::asio::async_write(
                 socket, boost::asio::buffer(resp.c_str(), resp.size()),
@@ -76,17 +79,17 @@ boost::cobalt::task<void> rx_process(Node& node,
             auto i = stoll(stream.substr(0, p)),
                  j = stoll(stream.substr(p + 1));
 
-            auto resp = "sa:" + node.serialize(node.stream(i, j));
+            auto resp = "sa:" + node->serialize(node->stream(i, j));
             co_await boost::asio::async_write(
                 socket, boost::asio::buffer(resp.c_str(), resp.size()),
                 boost::cobalt::use_task);
         } else if (cmd == "st") {
-            auto resp = "sta:" + node.get_status();
+            auto resp = "sta:" + node->get_status();
             co_await boost::asio::async_write(
                 socket, boost::asio::buffer(resp.c_str(), resp.size()),
                 boost::cobalt::use_task);
         } else if (cmd == "ring") {
-            const auto& [lookup, hash_lookup] = node.get_ring_view();
+            const auto& [lookup, hash_lookup] = node->get_ring_view();
             vector<pair<const string, const uint64_t>> bars;
             for (auto it = lookup.begin(); it != lookup.end(); ++it) {
                 const auto [token, timestamp, id_hash] = *it;
@@ -119,36 +122,36 @@ boost::cobalt::task<void> rx_process(Node& node,
             }
             replacement += "}";
 
-            {
-                string filename("web/ring_fmt.html");
+            // {
+            //     string filename("web/ring_fmt.html");
 
-                // Open the file for reading
-                std::ifstream inFile(filename);
-                assert(inFile);
+            //     // Open the file for reading
+            //     std::ifstream inFile(filename);
+            //     assert(inFile);
 
-                // Read the file content into a string
-                std::string fileContent(
-                    (std::istreambuf_iterator<char>(inFile)),
-                    std::istreambuf_iterator<char>());
-                inFile.close();
+            //     // Read the file content into a string
+            //     std::string fileContent(
+            //         (std::istreambuf_iterator<char>(inFile)),
+            //         std::istreambuf_iterator<char>());
+            //     inFile.close();
 
-                // Find and replace the target line with the replacement
-                string targetLine = "LINE_TO_REPLACE";
-                // string replacement = "blah";
-                if (fileContent.find(targetLine) != std::string::npos) {
-                    boost::algorithm::replace_all(fileContent, targetLine,
-                                                  replacement);
-                } else {
-                    assert(0);
-                }
+            //     // Find and replace the target line with the replacement
+            //     string targetLine = "LINE_TO_REPLACE";
+            //     // string replacement = "blah";
+            //     if (fileContent.find(targetLine) != std::string::npos) {
+            //         boost::algorithm::replace_all(fileContent, targetLine,
+            //                                       replacement);
+            //     } else {
+            //         assert(0);
+            //     }
 
-                // Open the file for writing and overwrite the content
-                std::ofstream outFile("web/ring.html");
-                assert(outFile);
+            //     // Open the file for writing and overwrite the content
+            //     std::ofstream outFile("web/ring.html");
+            //     assert(outFile);
 
-                outFile << fileContent;
-                outFile.close();
-            }
+            //     outFile << fileContent;
+            //     outFile.close();
+            // }
 
             auto resp = "ring_ack:" + replacement;
             co_await boost::asio::async_write(
@@ -165,14 +168,14 @@ boost::cobalt::task<void> rx_process(Node& node,
 //     co_await cancelTimer.async_wait(redirect_error(use_awaitable, ec));
 // }
 
-boost::cobalt::task<void> node_listener(Node& node) {
+boost::cobalt::task<void> node_listener(shared_ptr<Node> node) {
     auto executor = co_await boost::cobalt::this_coro::executor;
 
     // auto cancellation = co_await asio::this_coro::cancellation_state;
 
-    auto p = node.get_addr().find(":");
-    auto addr = node.get_addr().substr(0, p);
-    auto port = node.get_addr().substr(p + 1);
+    auto p = node->get_addr().find(":");
+    auto addr = node->get_addr().substr(0, p);
+    auto port = node->get_addr().substr(p + 1);
 
     boost::asio::steady_timer cancelTimer{
         executor, std::chrono::steady_clock::duration::max()};
@@ -203,13 +206,13 @@ boost::cobalt::task<void> node_listener(Node& node) {
 static queue<array<string, 2>> pending_add;
 static queue<string> pending_remove;
 
-boost::cobalt::task<void> heartbeat(map<string, shared_ptr<Node>>& nodes) {
+boost::cobalt::task<void> heartbeat(shared_ptr<Cluster> cluster) {
     auto io = co_await boost::cobalt::this_coro::executor;
     boost::asio::steady_timer timer(io);
 
     for (;;) {
 
-        for (auto& n : nodes) {
+        for (auto& n : (*cluster)) {
             co_await n.second->heartbeat();
         }
 
@@ -217,9 +220,9 @@ boost::cobalt::task<void> heartbeat(map<string, shared_ptr<Node>>& nodes) {
             auto [server, seed] = pending_add.front();
             pending_add.pop();
 
-            auto n = nodes[server] =
+            auto n = (*cluster)[server] =
                 std::shared_ptr<Node>(new Node(server, seed, 1));
-            boost::cobalt::spawn(io, node_listener(*n), boost::asio::detached);
+            boost::cobalt::spawn(io, node_listener(n), boost::asio::detached);
         }
 
         while (pending_remove.size()) {
@@ -232,7 +235,7 @@ boost::cobalt::task<void> heartbeat(map<string, shared_ptr<Node>>& nodes) {
 }
 
 static int port = 6000;
-boost::cobalt::task<void> cp_process(map<string, shared_ptr<Node>>& nodes,
+boost::cobalt::task<void> cp_process(shared_ptr<Cluster> cluster,
                                      boost::asio::ip::tcp::socket socket) {
     auto executor = co_await boost::cobalt::this_coro::executor;
     for (;;) {
@@ -257,8 +260,8 @@ boost::cobalt::task<void> cp_process(map<string, shared_ptr<Node>>& nodes,
                 boost::cobalt::use_task);
         } else if (cmd == "rn") {
             auto seed = payload.substr(p + 1);
-            auto& target = (*nodes.begin()).second->cancel_signal;
-            target.emit(boost::asio::cancellation_type::all);
+            // auto& target = (*nodes.begin()).second->cancel_signal;
+            // target.emit(boost::asio::cancellation_type::all);
 
             /* TODO: delete heartbeat */
             /* TODO: wait for all current connnections to drain */
@@ -267,15 +270,14 @@ boost::cobalt::task<void> cp_process(map<string, shared_ptr<Node>>& nodes,
     co_return;
 }
 
-boost::cobalt::task<void>
-system_listener(map<string, shared_ptr<Node>>& nodes) {
+boost::cobalt::task<void> system_listener(shared_ptr<Cluster> cluster) {
     auto executor = co_await boost::cobalt::this_coro::executor;
 
     boost::asio::ip::tcp::acceptor acceptor(executor,
                                             {boost::asio::ip::tcp::v4(), 5001});
     for (;;) {
         auto socket = co_await acceptor.async_accept(boost::cobalt::use_task);
-        boost::cobalt::spawn(executor, cp_process(nodes, std::move(socket)),
+        boost::cobalt::spawn(executor, cp_process(cluster, std::move(socket)),
                              boost::asio::detached);
     }
 
@@ -290,27 +292,30 @@ int main() {
         boost::asio::signal_set signals(io_context, SIGINT, SIGTERM);
         signals.async_wait([&](auto, auto) { io_context.stop(); });
 
+        cluster = shared_ptr<Cluster>(new Cluster());
+
         string addr = "127.0.0.1";
         int port = 5555;
         string seed;
         for (auto i = 0; i < NODES; ++i) {
 
             string addr_port = addr + ":" + to_string(port++);
-            auto n = nodes[addr_port] =
+            auto n = (*cluster)[addr_port] =
                 std::shared_ptr<Node>(new Node(addr_port, seed, 1));
             if (seed == "") {
                 seed = addr_port;
             }
             /* node control plane */
-            spawn(io_context, node_listener(*n), boost::asio::detached);
+            boost::cobalt::spawn(io_context, node_listener(n),
+                                 boost::asio::detached);
         }
 
         // /* distributed system heartbeat */
-        boost::cobalt::spawn(io_context, heartbeat(nodes),
+        boost::cobalt::spawn(io_context, heartbeat(cluster),
                              boost::asio::detached);
 
         // /* distributed system control plane */
-        boost::cobalt::spawn(io_context, system_listener(nodes),
+        boost::cobalt::spawn(io_context, system_listener(cluster),
                              boost::asio::detached);
 
         io_context.run();
