d9cfef7cc06695db90bdf1413fd57ff6d4158f01
Richard <code.monkey.ricky@gmail.com>
2024-11-26 10:41:29 -0800

    wip

--- node.hh
+++ node.hh
@@ -122,7 +122,7 @@ struct NodeMap {
 
 class Partitioner {
 
-    uint64_t range = 32;
+    uint64_t range = 1e9 + 7;
 
   public:
     Partitioner() { srand(0); }
@@ -423,7 +423,8 @@ class Node final {
     boost::cobalt::task<std::string> read_remote(std::string& peer,
                                                  std::string& key) {
 
-        std::cout << self << ":" << "read_remote() invoked!" << std::endl;
+        std::cout << self << ":" << "read_remote():" << peer << " - " << key
+                  << std::endl;
 
         auto io = co_await boost::asio::this_coro::executor;
 
@@ -503,8 +504,6 @@ class Node final {
 
         auto io = co_await boost::asio::this_coro::executor;
 
-        std::cout << self << ":" << "write_remote() invoked ##!" << std::endl;
-
         const auto p = peer_addr.find(":");
         const auto addr = peer_addr.substr(0, p);
         const auto port = peer_addr.substr(p + 1);
@@ -513,20 +512,43 @@ class Node final {
         boost::asio::ip::tcp::socket socket(io);
         auto ep = resolver.resolve(addr, port);
 
-        boost::asio::async_connect(
-            socket, ep,
-            [&socket](const boost::system::error_code& error,
-                      const boost::asio::ip::tcp::endpoint&) {});
+        try {
 
-        const auto payload = "wf:" + key + "=" + value;
-        co_await boost::asio::async_write(
-            socket, boost::asio::buffer(payload, payload.size()),
-            boost::cobalt::use_task);
+            boost::asio::async_connect(
+                socket, ep,
+                [&socket](const boost::system::error_code& error,
+                          const boost::asio::ip::tcp::endpoint&) {});
+
+            std::cout << self << ":" << "write_remote(): writing to "
+                      << peer_addr << std::endl;
+
+            const auto payload = "wf:" + key + "=" + value;
+            co_await boost::asio::async_write(
+                socket, boost::asio::buffer(payload, payload.size()),
+                boost::cobalt::use_task);
+
+            std::cout << self << ":" << "write_remote(): waiting for ack... "
+                      << std::endl;
+
+            char rx[1024] = {};
+            auto n = co_await socket.async_read_some(boost::asio::buffer(rx),
+                                                     boost::cobalt::use_task);
+
+            std::cout << self << ":" << "write_remote(): ack received! "
+                      << std::endl;
+        } catch (const std::exception& e) {
+            std::cerr << "Connection error: " << e.what() << std::endl;
+            assert(0);
+        }
+
+        volatile int dummy = 0;
     }
 
     boost::cobalt::task<void> write(const std::string& key,
                                     const std::string& value,
                                     bool coordinator = true) {
+        std::cout << "write(): " << key << "=" << value << ", " << coordinator
+                  << std::endl;
 
         auto key_hash =
             static_cast<uint64_t>(std::hash<std::string>{}(key)) % p.getRange();
@@ -542,7 +564,7 @@ class Node final {
         auto it = lookup.lower_bound(target);
 
         /* walk the ring until we find a working node */
-        int rf = replication_factor;
+        int rf = 1; /* TODO */ // replication_factor;
         while (true) {
 
             /* wrap around if needed */
@@ -618,148 +640,155 @@ class Node final {
 
     boost::cobalt::task<void> rx_process(boost::asio::ip::tcp::socket socket) {
 
-        std::cout << self << ":" << "rx_process() spawned!" << std::endl;
+        // std::cout << self << ":" << "rx_process() spawned!" << std::endl;
 
         ++outstanding;
 
-        // for (;;)
-        {
-            char data[1024] = {};
-            std::size_t n = co_await socket.async_read_some(
-                boost::asio::buffer(data), boost::cobalt::use_task);
+        try {
+            for (;;) {
+                char data[1024] = {};
+                std::size_t n = co_await socket.async_read_some(
+                    boost::asio::buffer(data), boost::cobalt::use_task);
 
-            std::string payload = std::string(data, n);
-            auto p = payload.find(":");
+                std::string payload = std::string(data, n);
+                auto p = payload.find(":");
 
-            auto cmd = payload.substr(0, p);
-            if (cmd == "r") {
-                /* read */
-                auto key = std::string(data + 2, n - 2);
-                auto value = co_await read(key);
-                auto resp = "ra:" + value;
-                co_await boost::asio::async_write(
-                    socket, boost::asio::buffer(resp.c_str(), resp.size()),
-                    boost::cobalt::use_task);
+                auto cmd = payload.substr(0, p);
+                if (cmd == "r") {
+                    /* read */
+                    auto key = std::string(data + 2, n - 2);
+                    auto value = co_await read(key);
+                    auto resp = "ra:" + value;
+                    co_await boost::asio::async_write(
+                        socket, boost::asio::buffer(resp.c_str(), resp.size()),
+                        boost::cobalt::use_task);
 
-            } else if (cmd == "w" || cmd == "wf") {
+                } else if (cmd == "w" || cmd == "wf") {
 
-                /* write */
+                    /* write */
 
-                auto kv =
-                    std::string(data + cmd.size() + 1, n - cmd.size() - 1);
-                auto p = kv.find("=");
-                auto k = kv.substr(0, p);
-                auto v = kv.substr(p + 1);
+                    auto kv =
+                        std::string(data + cmd.size() + 1, n - cmd.size() - 1);
+                    auto p = kv.find("=");
+                    auto k = kv.substr(0, p);
+                    auto v = kv.substr(p + 1);
 
-                co_await write(k, v, cmd == "w");
+                    co_await write(k, v, cmd == "w");
 
-                std::string resp;
-                if (cmd == "wf")
-                    resp = std::string("wfa:");
-                else
-                    resp = std::string("wa:");
+                    std::string resp;
+                    if (cmd == "wf")
+                        resp = std::string("wfa:");
+                    else
+                        resp = std::string("wa:");
 
-                co_await boost::asio::async_write(
-                    socket, boost::asio::buffer(resp.c_str(), resp.size()),
-                    boost::cobalt::use_task);
-            } else if (cmd == "g") {
-                /* gossip */
-                auto gossip = payload.substr(p + 1);
-                gossip_rx(gossip);
-                auto resp = "ga:" + gossip;
-                co_await boost::asio::async_write(
-                    socket, boost::asio::buffer(resp.c_str(), resp.size()),
-                    boost::cobalt::use_task);
-            } else if (cmd == "s") {
-                auto s = payload.substr(p + 1);
-                auto p = s.find("-");
-                auto i = stoll(s.substr(0, p)), j = stoll(s.substr(p + 1));
+                    co_await boost::asio::async_write(
+                        socket, boost::asio::buffer(resp.c_str(), resp.size()),
+                        boost::cobalt::use_task);
+                } else if (cmd == "g") {
+                    /* gossip */
+                    auto gossip = payload.substr(p + 1);
+                    gossip_rx(gossip);
+                    auto resp = "ga:" + gossip;
+                    co_await boost::asio::async_write(
+                        socket, boost::asio::buffer(resp.c_str(), resp.size()),
+                        boost::cobalt::use_task);
+                } else if (cmd == "s") {
+                    auto s = payload.substr(p + 1);
+                    auto p = s.find("-");
+                    auto i = stoll(s.substr(0, p)), j = stoll(s.substr(p + 1));
 
-                auto resp = "sa:" + serialize(stream(i, j));
-                co_await boost::asio::async_write(
-                    socket, boost::asio::buffer(resp.c_str(), resp.size()),
-                    boost::cobalt::use_task);
-            } else if (cmd == "st") {
-                auto resp = "sta:" + get_status();
-                co_await boost::asio::async_write(
-                    socket, boost::asio::buffer(resp.c_str(), resp.size()),
-                    boost::cobalt::use_task);
-            } else if (cmd == "ring") {
-                const auto& [lookup, hash_lookup] = get_ring_view();
-                std::vector<std::pair<const std::string, const uint64_t>> bars;
-                for (auto it = lookup.begin(); it != lookup.end(); ++it) {
-                    const auto [token, timestamp, id_hash] = *it;
-                    auto p =
-                        it == lookup.begin() ? prev(lookup.end()) : prev(it);
-                    auto [ptoken, skip, skip1] = *p;
-                    auto range =
-                        it != lookup.begin()
-                            ? (token - ptoken)
-                            : (token + Partitioner::instance().getRange() -
-                               ptoken);
-                    auto s = hash_lookup.at(id_hash);
-                    bars.push_back(make_pair(s, range));
+                    auto resp = "sa:" + serialize(stream(i, j));
+                    co_await boost::asio::async_write(
+                        socket, boost::asio::buffer(resp.c_str(), resp.size()),
+                        boost::cobalt::use_task);
+                } else if (cmd == "st") {
+                    auto resp = "sta:" + get_status();
+                    co_await boost::asio::async_write(
+                        socket, boost::asio::buffer(resp.c_str(), resp.size()),
+                        boost::cobalt::use_task);
+                } else if (cmd == "ring") {
+                    const auto& [lookup, hash_lookup] = get_ring_view();
+                    std::vector<std::pair<const std::string, const uint64_t>>
+                        bars;
+                    for (auto it = lookup.begin(); it != lookup.end(); ++it) {
+                        const auto [token, timestamp, id_hash] = *it;
+                        auto p = it == lookup.begin() ? prev(lookup.end())
+                                                      : prev(it);
+                        auto [ptoken, skip, skip1] = *p;
+                        auto range =
+                            it != lookup.begin()
+                                ? (token - ptoken)
+                                : (token + Partitioner::instance().getRange() -
+                                   ptoken);
+                        auto s = hash_lookup.at(id_hash);
+                        bars.push_back(make_pair(s, range));
 
-                    /*
-                     * format to:
-                     * var data = {a: 9, b: 20, c:30, d:8, e:12, f:3, g:7, h:14}
-                     */
+                        /*
+                         * format to:
+                         * var data = {a: 9, b: 20, c:30, d:8, e:12, f:3, g:7,
+                         * h:14}
+                         */
+                    }
+
+                    std::unordered_map<std::string, int> cnt;
+
+                    std::string replacement = "{";
+                    int k = 0;
+                    for (auto& bar : bars) {
+                        auto p = bar.first.find(":");
+                        auto name = bar.first.substr(p + 1);
+                        /* <index>-<node>-<instance>*/
+                        replacement += "\"" + std::to_string(k++) + "-" + name +
+                                       "-" + std::to_string(cnt[name]++) +
+                                       "\"" + ":" + std::to_string(bar.second) +
+                                       ",";
+                    }
+                    replacement += "}";
+
+                    // {
+                    //     string filename("web/ring_fmt.html");
+
+                    //     // Open the file for reading
+                    //     std::ifstream inFile(filename);
+                    //     assert(inFile);
+
+                    //     // Read the file content into a string
+                    //     std::string fileContent(
+                    //         (std::istreambuf_iterator<char>(inFile)),
+                    //         std::istreambuf_iterator<char>());
+                    //     inFile.close();
+
+                    //     // Find and replace the target line with the
+                    //     replacement string targetLine = "LINE_TO_REPLACE";
+                    //     // string replacement = "blah";
+                    //     if (fileContent.find(targetLine) !=
+                    //     std::string::npos) {
+                    //         boost::algorithm::replace_all(fileContent,
+                    //         targetLine,
+                    //                                       replacement);
+                    //     } else {
+                    //         assert(0);
+                    //     }
+
+                    //     // Open the file for writing and overwrite the
+                    //     content std::ofstream outFile("web/ring.html");
+                    //     assert(outFile);
+
+                    //     outFile << fileContent;
+                    //     outFile.close();
+                    // }
+
+                    auto resp = "ring_ack:" + replacement;
+                    co_await boost::asio::async_write(
+                        socket, boost::asio::buffer(resp.c_str(), resp.size()),
+                        boost::cobalt::use_task);
                 }
-
-                std::unordered_map<std::string, int> cnt;
-
-                std::string replacement = "{";
-                int k = 0;
-                for (auto& bar : bars) {
-                    auto p = bar.first.find(":");
-                    auto name = bar.first.substr(p + 1);
-                    /* <index>-<node>-<instance>*/
-                    replacement += "\"" + std::to_string(k++) + "-" + name +
-                                   "-" + std::to_string(cnt[name]++) + "\"" +
-                                   ":" + std::to_string(bar.second) + ",";
-                }
-                replacement += "}";
-
-                // {
-                //     string filename("web/ring_fmt.html");
-
-                //     // Open the file for reading
-                //     std::ifstream inFile(filename);
-                //     assert(inFile);
-
-                //     // Read the file content into a string
-                //     std::string fileContent(
-                //         (std::istreambuf_iterator<char>(inFile)),
-                //         std::istreambuf_iterator<char>());
-                //     inFile.close();
-
-                //     // Find and replace the target line with the replacement
-                //     string targetLine = "LINE_TO_REPLACE";
-                //     // string replacement = "blah";
-                //     if (fileContent.find(targetLine) != std::string::npos) {
-                //         boost::algorithm::replace_all(fileContent,
-                //         targetLine,
-                //                                       replacement);
-                //     } else {
-                //         assert(0);
-                //     }
-
-                //     // Open the file for writing and overwrite the content
-                //     std::ofstream outFile("web/ring.html");
-                //     assert(outFile);
-
-                //     outFile << fileContent;
-                //     outFile.close();
-                // }
-
-                auto resp = "ring_ack:" + replacement;
-                co_await boost::asio::async_write(
-                    socket, boost::asio::buffer(resp.c_str(), resp.size()),
-                    boost::cobalt::use_task);
             }
+        } catch (const std::exception& e) {
+            // std::cerr << "Connection error: " << e.what() << std::endl;
         }
 
-        std::cout << self << ":" << "rx_process() end!" << std::endl;
+        // std::cout << self << ":" << "rx_process() end!" << std::endl;
 
         --outstanding;
 
