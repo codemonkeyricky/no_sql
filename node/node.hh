f7a468e028c68d13babf3d05826e3a51ad98b8d8
Richard Tang <code.monkey.ricky@gmail.com>
2024-12-14 22:45:05 -0800

    wip

--- node/node.hh
+++ node/node.hh
@@ -186,16 +186,8 @@ class Node final {
     // ~Node() {}
 
     /* stream data: [i, j) */
-    std::map<hash, std::pair<key, value>> stream(hash i, hash j) {
 
-        std::map<hash, std::pair<key, value>> rv;
-        for (auto it = db.lower_bound(i); it != db.end() && it->first < j;
-             ++it) {
-            rv[it->first] = it->second;
-        }
-
-        return rv;
-    }
+    std::map<hash, std::pair<key, value>> stream(hash i, hash j);
 
     template <typename T> std::string serialize(T&& data);
 
@@ -203,123 +195,7 @@ class Node final {
 
     void gossip_rx(std::string& gossip);
 
-    boost::cobalt::task<void> gossip_tx() {
-
-        // std::cout << self << ":" << "gossip_tx() - #1" << std::endl;
-
-        /* collect all peers */
-        std::vector<node_addr> peers;
-        for (auto& peer : local_map.nodes) {
-            /* exclude ourselves */
-            if (peer.first != self) {
-                if (peer.second.status != NodeMap::Node::Down) {
-                    peers.push_back(peer.first);
-                }
-            }
-        }
-
-        /* TODO: pick 3 peers at random to gossip */
-        int k = 1;
-
-        auto io = co_await boost::asio::this_coro::executor;
-        // std::cout << self << ":" << "gossip_tx() - #2" << std::endl;
-        while (k && peers.size()) {
-            auto kk = peers[rand() % peers.size()];
-
-            auto peer_addr = kk;
-            auto p = peer_addr.find(":");
-            auto addr = peer_addr.substr(0, p);
-            auto port = peer_addr.substr(p + 1);
-
-            boost::asio::ip::tcp::resolver resolver(io);
-            boost::asio::ip::tcp::socket socket(io);
-            auto ep = resolver.resolve(addr, port);
-
-            boost::system::error_code err_code;
-            boost::asio::async_connect(
-                socket, ep,
-                [&socket, &err_code](const boost::system::error_code& error,
-                                     const boost::asio::ip::tcp::endpoint&) {
-                    err_code = error;
-                    // std::cout << "error = " << error << std::endl;
-                });
-
-            // std::cout << self << ":" << "gossip_tx() - #3" << std::endl;
-            try {
-
-                auto payload = "g:" + serialize(local_map);
-                co_await boost::asio::async_write(
-                    socket, boost::asio::buffer(payload, payload.size()),
-                    boost::cobalt::use_task);
-
-                /* read results */
-
-                // std::cout << self << ":" << "gossip_tx() - #4" << std::endl;
-                char rx_payload[1024] = {};
-                std::size_t n = co_await socket.async_read_some(
-                    boost::asio::buffer(rx_payload), boost::cobalt::use_task);
-
-                auto serialized_data = std::string(rx_payload + 3, n - 3);
-                auto remote_map = deserialize<NodeMap>(serialized_data);
-                local_map = remote_map = local_map + remote_map;
-                update_lookup();
-
-                /* TODO: account for peer death */
-
-            } catch (std::exception& e) {
-                std::cout << self << ":" << "heartbeat() - failed to connect "
-                          << peer_addr << std::endl;
-                local_map.nodes[peer_addr].status = NodeMap::Node::Down;
-                local_map.nodes[peer_addr].timestamp = current_time_ms();
-                update_lookup();
-            }
-            --k;
-        }
-
-        if (local_map.nodes[self].status == NodeMap::Node::Joining) {
-
-            auto& my_node = local_map.nodes[self];
-
-            /* take over token, token-1, token-2, .... ptoken +1*/
-
-            if (lookup.size()) {
-
-                for (auto& my_token : my_node.tokens) {
-
-                    /*
-                     * range to stream is from previous token to my_token
-                     * from the *next* node.
-                     */
-
-                    /* calculate range */
-
-                    std::array<uint64_t, 3> target = {my_token};
-                    auto it = lookup.lower_bound(target);
-                    if (it == lookup.end()) {
-                        it = lookup.begin();
-                    }
-                    auto [token, timestamp, id] = *it;
-
-                    Lookup::iterator p;
-                    if (it == lookup.begin())
-                        p = prev(lookup.end());
-                    else
-                        p = prev(it);
-
-                    auto [ptoken, pts, pid] = *p;
-
-                    auto remote_db = co_await stream_from_remote(
-                        nodehash_lookup[id], ptoken + 1, token);
-
-                    /* insert into local db */
-                    db.insert(remote_db.begin(), remote_db.end());
-                }
-            }
-
-            local_map.nodes[self].status = NodeMap::Node::Live;
-            local_map.nodes[self].timestamp = current_time_ms();
-        }
-    }
+    boost::cobalt::task<void> gossip_tx();
 
     boost::cobalt::task<std::pair<boost::system::error_code, std::string>>
     read_remote(std::string& peer, std::string& key) {
