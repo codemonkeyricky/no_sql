a5274cc9714079e904d46d2b77fc233bc7709af9
Richard Tang <code.monkey.ricky@gmail.com>
2024-12-21 22:16:54 -0800

    snapshot

--- node/test/replica.cc
+++ node/test/replica.cc
@@ -1,5 +1,9 @@
 #include "node/replica.hh"
 
+#include <boost/algorithm/string.hpp>
+#include <boost/asio.hpp>
+#include <boost/cobalt.hpp>
+
 #include <string>
 #include <vector>
 
@@ -7,9 +11,11 @@ using namespace std;
 
 int main() {
 
-    // vector<string> cluster = {"127.0.0.1:5555", "127.0.0.1:5556"};
+    vector<string> cluster = {"127.0.0.1:5555", "127.0.0.1:5556"};
 
-    // Replica r0(cluster[0], cluster);
+    Replica r0(cluster[0], cluster);
+
+    boost::asio::io_context io(1);
 
     /* TC: All followers electing one leader */
 
